dafdf8e5a24550d459624cb7ba165b55a0a8b75e
Vaibhav <vaibhavpandeyprayag@gmail.com>
2024-02-26 02:26:09 +0530

    Skills section added

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function initialize() {
   document.querySelector("#experienceVal").innerHTML = `${
     years == 1 ? `${years} year` : years > 1 ? `${years} years` : ""
   } ${months == 1 ? `${months} month` : months > 1 ? `${months} months` : ""}`;
+
+  window.addEventListener("scroll", setNavBar);
 }
 
 function calculateExperience() {
@@ -19,4 +21,17 @@ function calculateExperience() {
   const diffInYears = Math.floor(diffInMonths / 12);
 
   return { months: diffInMonths % 12, years: diffInYears };
+}
+
+function setNavBar() {
+  if (window.scrollY >= 25) {
+    // my-md-5
+    document.querySelector("#navBar").classList.remove("my-md-5");
+    document.querySelector("#navBar").classList.add("bg-black");
+    console.log(">= 15");
+  } else {
+    document.querySelector("#navBar").classList.add("my-md-5");
+    document.querySelector("#navBar").classList.remove("bg-black");
+    console.log("< 15");
+  }
 }
